3cde958c5e629a1fee2d6bec881e924cd49e9f1c
SonlyF5020 <502089948@163.com>
2013-08-25 23:40:55 +0800

    build a database map to interface with data

--- src/main/java/JDBCfirst.java
+++ src/main/java/JDBCfirst.java
@@ -11,12 +11,35 @@ public class JDBCfirst {
         statement = connection.createStatement();
         resultSet = statement.executeQuery("select * from test.vaboo");
         writeResult(resultSet);
+        connection.close();
     }
+
 
     private void writeResult(ResultSet resultSet) throws SQLException {
         while (resultSet.next()) {
             System.out.println(resultSet.getString("yourID"));
             System.out.println(resultSet.getString("yourName"));
+            System.out.println(resultSet.getString("pointID"));
         }
+    }
+
+    public void put(int key, String value) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/test", "twer", "");
+        statement = connection.createStatement();
+        String insertSentense = "insert into test.map values("+key+",'"+value+"');";
+        statement.execute(insertSentense);
+        connection.close();
+    }
+
+    public String get(int key) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/test","twer","");
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery("select * from test.map");
+        if (resultSet.next()){
+            return resultSet.getString(2);
+        }
+        return "";
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         JDBCfirst jdbCfirst = new JDBCfirst();
         jdbCfirst.readDataBase();
+
+        jdbCfirst.put(5020, "mei");
+        String value = jdbCfirst.get(5020);
+        System.out.println(value);
     }
 }
